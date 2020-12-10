ad68fed98fb41ff13dd2c363c72cae33e80e10a5
Christoffel Angga <chris.ang.dev@gmail.com>
2020-12-10 12:38:36 -0800

    First commit, level 0 done

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
-
+var movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
 const App = (props) => (
-  <div>Hello World!</div>
+  movies.map((movie, idx) =>
+  <div key={movie.title + idx} className= 'movie-list'>{movie.title}</div>
+  )
 );
 
 export default App;
