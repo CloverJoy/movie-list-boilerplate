cf03ec2b1d0279d1e381f9dc1f8e7cab0ffe93e2
Christoffel Angga <chris.ang.dev@gmail.com>
2021-01-10 13:35:13 -0800

    Optimize + level 1 with conditional rendering

--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ const PORT = 3000 || process.env.PORT;
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
+  console.log(`Server listening on port: http://localhost:${PORT}`);
 })
